04d61c5ff69573834b0937dc32db5386505dd761
postrel63 <postrel63@naver.com>
2024-08-19 12:43:43 +0900

    first init

---
+++ src/main/java/com/example/docker03/Docker03Application.java
@@ -0,0 +1,13 @@
+package com.example.docker03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Docker03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Docker03Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/docker03/Docker03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.docker03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Docker03ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
